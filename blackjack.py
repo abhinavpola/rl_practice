bbc662297eb7c39aaa433d65c7f1ad750194ae60
Abhinav Pola <abhinavpola@google.com>
2024-09-17 09:47:02 -0700

    q-learning tutorial blackjack agent

---
+++ blackjack.py
@@ -0,0 +1,125 @@
+import gymnasium as gym
+from collections import defaultdict
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+class BlackjackAgent:
+    def __init__(
+        self,
+        env,
+        learning_rate,
+        initial_epsilon,
+        epsilon_decay,
+        final_epsilon,
+        discount_factor: float = 0.95,
+    ) -> None:
+        self.env = env
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+        # exploration
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        # exploitation
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update(
+        self,
+        obs: tuple[int, int, bool],
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_obs: tuple[int, int, bool],
+    ) -> None:
+        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        temporal_difference = (
+            reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+        )
+
+        self.q_values[obs][action] += self.lr * temporal_difference
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+
+def main():
+    learning_rate = 0.01
+    n_episodes = 100_000
+    start_epsilon = 1.0
+    epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+    final_epsilon = 0.1
+    env = gym.make("Blackjack-v1", sab=True, render_mode='human')
+
+
+    agent = BlackjackAgent(
+        env=env,
+        learning_rate=learning_rate,
+        initial_epsilon=start_epsilon,
+        epsilon_decay=epsilon_decay,
+        final_epsilon=final_epsilon,
+    )
+
+    env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
+
+    for episode in tqdm(range(n_episodes)):
+        obs, info = env.reset()
+        done = False
+
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            agent.update(obs, action, reward, terminated, next_obs)
+
+            done = terminated or truncated
+            obs = next_obs
+            env.render()
+        
+        agent.decay_epsilon()
+    
+    # plot(env, agent)
+    env.close()
+
+
+def plot(env, agent):
+    rolling_length = 500
+    fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
+    axs[0].set_title("Episode rewards")
+    # compute and assign a rolling average of the data to provide a smoother graph
+    reward_moving_average = (
+        np.convolve(
+            np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
+        )
+        / rolling_length
+    )
+    axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+    axs[1].set_title("Episode lengths")
+    length_moving_average = (
+        np.convolve(
+            np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
+        )
+        / rolling_length
+    )
+    axs[1].plot(range(len(length_moving_average)), length_moving_average)
+    axs[2].set_title("Training Error")
+    training_error_moving_average = (
+        np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
+        / rolling_length
+    )
+    axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
